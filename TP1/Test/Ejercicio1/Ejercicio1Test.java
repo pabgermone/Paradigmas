4c1d9bccd4f237aa6f38b156edbc9f0efc5177da
pabgermone <pabgermone@gmail.com>
2016-09-07 11:11:38 -0300

    TP1

--- TP1/Test/Ejercicio1/Ejercicio1Test.java
+++ TP1/Test/Ejercicio1/Ejercicio1Test.java
@@ -1,19 +1,26 @@
 package Ejercicio1;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class Ejercicio1Test {
+    Recta recta;
+
+    @Before
+    public void setUp(){
+        recta = new Recta();
+    }
 
     @Test
     public void probarPunto() throws Exception {
-        assertTrue(new Recta().probarPunto(1, 10));
+        assertTrue(recta.probarPunto(1, 10));
     }
 
     @Test
     public void probarPunto2() throws Exception{
-        assertFalse(new Recta().probarPunto(2,10));
+        assertFalse(recta.probarPunto(2,10));
     }
 
 }
