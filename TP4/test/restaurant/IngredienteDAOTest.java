d39531d295069b24ce11adaf9189edab80b4f94a
pabgermone <pabgermone@gmail.com>
2016-09-18 02:28:25 -0300

    TP4

---
+++ TP4/test/restaurant/IngredienteDAOTest.java
@@ -0,0 +1,43 @@
+package restaurant;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Pablo Germone
+ */
+public class IngredienteDAOTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void altaIngrediente() throws Exception {
+
+    }
+
+    @Test
+    public void borrarIngrediente() throws Exception {
+
+    }
+
+    @Test
+    public void modificarIngrediente() throws Exception {
+
+    }
+
+    @Test
+    public void devolverIngredientes() throws Exception {
+
+    }
+
+}
