615baf5652d4929300e4a02e2c19c8321629aa92
pabgermone <pabgermone@gmail.com>
2016-10-06 12:47:04 -0300

    TP6

--- TP6/src/ejercicio1/NumeroComplejo.java
+++ TP6/src/ejercicio1/NumeroComplejo.java
@@ -4,23 +4,15 @@ package ejercicio1;
  * @author Pablo Germone
  */
 public class NumeroComplejo {
-    private String[] partes;
-
-    private int parteReal;
-    private int valorImaginario;
-    private String parteImaginaria;
-
     private String valor;
-
+    private int parteReal;
+    private String parteImaginaria;
+    private int valorImaginario;
 
 
     public NumeroComplejo(String valor){
         setValor(valor);
-
         setPartes();
-        setParteReal();
-        setParteImaginaria();
-        setValorImaginario();
     }
 
 
@@ -29,19 +21,11 @@ public class NumeroComplejo {
         return parteReal;
     }
 
-    private void setParteReal() {
-        this.parteReal = Integer.parseInt(partes[0]);
-    }
-
     public String getParteImaginaria() {
         return parteImaginaria;
     }
 
-    private void setParteImaginaria() {
-        this.parteImaginaria = partes[2];
-    }
-
-    private String getValor() {
+    public String getValor() {
         return valor;
     }
 
@@ -49,22 +33,71 @@ public class NumeroComplejo {
         this.valor = valor;
     }
 
-    private void setPartes(){
-        partes = getValor().split("//s");
-    }
-
     public int getValorImaginario() {
         return valorImaginario;
     }
 
+    /**
+     * Separa el String de la parte imaginaria, quedandose solo con el valor numerico
+     */
     private void setValorImaginario() {
         String[] imaginario = getParteImaginaria().split("[0-9]");
         valorImaginario = Integer.parseInt(imaginario[0]);
     }
 
+    /**
+     * Toma las partes del numero complejo y las asigna a las propiedades correspondientes
+     */
+    private void setPartes(){
+        String[] partes = getValor().split("//s");
+        this.parteReal = Integer.parseInt(partes[0]);
+        this.parteImaginaria = partes[2];
+        setValorImaginario();
+    }
 
 
+    /**
+     * Suma el numero actual con otro numero complejo
+     * @param numero Numero con el que se quiere sumar
+     * @return Numero complejo resultante de la suma
+     */
     public NumeroComplejo suma(NumeroComplejo numero){
+        String respuesta;
+        int respuestaReal;
+        int respuestaImaginaria;
 
+        respuestaReal = this.getParteReal() + numero.getParteReal();
+        respuestaImaginaria = this.getValorImaginario() + numero.getValorImaginario();
+
+        if(respuestaImaginaria < 0){
+            respuesta = respuestaReal + " " + respuestaImaginaria + "i";
+        }else{
+            respuesta = respuestaReal + " + " + respuestaImaginaria + "i";
+        }
+
+        return new NumeroComplejo(respuesta);
+    }
+
+
+    /**
+     * Resta al numero actual otro numero complejo
+     * @param numero Numero complejo que se quiere restar
+     * @return Numero complejo resultante de la resta
+     */
+    public NumeroComplejo resta(NumeroComplejo numero){
+        String respuesta;
+        int respuestaReal;
+        int respuestaImaginaria;
+
+        respuestaReal = this.getParteReal() - numero.getParteReal();
+        respuestaImaginaria = this.getValorImaginario() - numero.getValorImaginario();
+
+        if(respuestaImaginaria < 0){
+            respuesta = respuestaReal + " " + respuestaImaginaria + "i";
+        }else{
+            respuesta = respuestaReal + " + " + respuestaImaginaria + "i";
+        }
+
+        return new NumeroComplejo(respuesta);
     }
 }
