61dcabcf687d7d7b8b32e863398e2347fd343784
pabgermone <pabgermone@gmail.com>
2016-11-20 18:03:55 -0300

    Paradigmas

---
+++ TP2/src/Pruebas.java
@@ -0,0 +1,22 @@
+import Ejercicio4.Potencia;
+import Ejercicio5.Clasificador;
+import Ejercicio9.Merge;
+
+public class Pruebas {
+
+    public static void main(String [] args){
+        /*
+        Clasificador clasif = new Clasificador();
+        int [] numeros = {1, 4, 7, 13, 19, 2, 3};
+
+        clasif.clasificarNumeros(numeros);*/
+
+        /*
+        Merge merge = new Merge();
+
+        merge.mergeArchivos("d:\\Paradigmas_TP2_Archivo1.txt", "d:\\Paradigmas_TP2_Archivo2.txt");*/
+
+        Potencia pot = new Potencia();
+        System.out.println(pot.encontrarNumero());
+    }
+}
